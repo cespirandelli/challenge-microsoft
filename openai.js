39e1633eab248af1efd8eeb6cc3372eef40b8965
Lucas Della Libera Alves <lucas.della@semantix.ai>
2023-09-25 11:52:32 -0300

    refatorando chamada pro ChatGPT

--- openai.js
+++ openai.js
@@ -1,26 +1,28 @@
-//Blibiotecas
 require('dotenv').config();
-
-//Declaração de variáveis
 const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
 const endpoint = process.env.AZURE_OPENAI_ENDPOINT;
 const azureApiKey = process.env.AZURE_OPENAI_KEY;
 const azureDeployment = process.env.AZURE_DEPLOYMENT_ID;
 
-// Configuração do ChatGPT
 const messages = [
-  { role: "system", content: "You are an assistent that help people find information." },
-  { role: "user", content: "Qual é o preço da carne?" },
+  { role: "system", content: "Você deve identificar na mensagem recebida o nome de produto ou comida e responder apenas isso. \
+                              Input Example: Quero comprar um litro de leite muito gostoso \
+                              Output Example: Leite" },
+  { role: "user", content: "Quero comprar arroz branco e fino" },
 ];
 
 async function main() {
   const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
   const deploymentId = azureDeployment;
-  const result = client.listChatCompletions(deploymentId, messages);
+  const result = await client.getChatCompletions(deploymentId, messages);
 
-  //PRECISO COLOCAR O RETORNO DO RESULTADO DO CHATGPT
+  for (const choice of result.choices) {
+    console.log(choice.message.content);
+  }
 }
 
 main().catch((err) => {
-    console.error("The sample encountered an error:", err);
-  });
+  console.error("The sample encountered an error:", err);
+});
+
+module.exports = { main };
